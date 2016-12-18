46d9d51fa6f055c7d0cb14932f1785d5dbe7f89c
J-Zhang104 <ussc95@gmail.com>
2016-12-18 11:43:03 -0500

    This is all the work that I have done so far.
    I finished the resume page, and experimented with a Javascript plugin
    for the portfolio page.

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Project 3</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/styles.css"> 
+</head>
+<body>
+
+<div class="container">
+
+<header>
+
+	<h1>Jack Zhang</h1>
+
+	<?php include "inc/nav.inc"; ?>
+
+</header>
+
+
+	<img id="img1" src="images/blue.jpg" alt="blue">
+
+<article>
+
+	<h2>About Me</h2>
+
+	<p>My name is Jack Zhang, and I am a sophomore at the University of Rochester. I recently completed CSC 174: Advanced Web Design and Development. I am planning on majoring in Political Science and minoring in Computer Science. I completed twelve lab assignments and three projects this semester.</p>
+
+</article>
+
+<aside>
+
+	<h2>Explore My Website!</h2>
+
+</aside>
+
+<section id="part2">
+
+<h2>Accomplishments</h2>
+
+<article>
+
+<h3>Labs</h3>
+
+<ul id="labs">
+	<li>Lab 1: Account Setup</li>
+	<li>Lab 2: PHP Includes</li>
+	<li>Lab 3: Front-end Frameworks</li>
+	<li>Lab 4: Github Setup</li>
+	<li>Lab 5: HTML5 Boilerplate Quiz</li>
+	<li>Lab 6: CSS Architecture</li>
+	<li>Lab 7: Web Fonts</li>
+	<li>Lab 8: Team Project Presentation</li>
+	<li>Lab 9: Principles of Page Design</li>
+	<li>Lab 10: JavaScript Form Validation</li>
+	<li>Lab 11: Wordpress (complete)</li>
+	<li>Lab 12: PHP and MySQL</li>
+</ul>
+
+<h3>Projects</h3>
+
+<ul id="projects">
+	<li>Project 1</li>
+	<li>Project 2</li>
+	<li>Project 3</li>
+</ul>
+
+<img id="img2" src="images/683992.jpg" alt="skyscrapers">
+
+<p class="description">The first two projects involved groups of four to five students. I happened to be in the group with only four people, but the project turned out well in the end. I handled the CSS for Project 1, and I had to work around and expand upon the styles in the CSS framework. Project 1 was also my first time using Github. Not only did Github cause some problems early on, but it was difficult to coordinate the efforts of all four group members. However, we succeeded in our joint endeavor and produced a working website on time.</p>
+
+<img id="img3" src="images/water.jpg" alt="water">
+
+</article>
+
+<aside>
+
+<p class="description">The labs in this class were challenging, and it took me a great deal of time to work through most of them. Some of the earlier labs were a review of concepts learned in CSC 170, but as new concepts were added, they became much more difficult. Lab 7 was my favorite lab because adding web fonts to a page makes it more unique and demonstrates a higher level of creativity. Lab 11 was my least favorite for several reasons. First, the back-end work on the web server was extremely frustrating because 99% of the time nothing worked. Second, I prefer building a webpage from scratch, rather than use a template and theme provided by Wordpress. Nonetheless, I think Wordpress is an excellent tool, and it has many cool features to play around with.</p>
+
+<img id="img4" src="images/775776.jpg" alt="lightning storm">
+
+</aside>
+
+</section>
+
+<footer>
+
+<h3>Created by Jack Zhang</h3>
+
+</footer>
+
+</div>
+
+</body>
+</html>
